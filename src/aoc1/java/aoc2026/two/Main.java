f6563cd900d535585466eac59a642655a88351f1
spongeMan <wspolpracaincei@gmail.com>
2026-04-02 18:13:49 +0200

    I am doing Part 2 of aoc2025. "Its harder".

---
+++ src/aoc1/java/aoc2026/two/Main.java
@@ -0,0 +1,17 @@
+package aoc2026.two;
+
+import static aoc2026.two.Ids.input;
+
+public class Main {
+    public static void main() {
+        String[] onePart = input.split(",");
+        String[] otherPart = input.split("-");
+
+        long sum = 0;
+
+        for(String range : onePart){
+
+        }
+
+    }
+}
